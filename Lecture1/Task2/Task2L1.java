be7867721725ff02627ab5bf94ca383cde2fc8a4
VictorLVA <victorl@playtika.com>
2018-11-08 11:14:26 +0300

    fixes

--- Lecture1/Task2/Task2L1.java
+++ Lecture1/Task2/Task2L1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Task2L1 {
     public static void main ( String[] args ) {
-        int mode = 0;
+        int mode;
         int minMark = 1;
         int maxMark = 5;
         Scanner in = new Scanner ( System.in );
@@ -10,198 +10,182 @@ public class Task2L1 {
         mode = in.nextInt ( );
         if (mode == 1) {
             float sumMark = 0;
-            float avgMark = 0;
-            int StudentCount = 5;
-            Student[] allstudents = new Student[ StudentCount ]; //зачем этот массив для решения задачи?
-            StudentMark[] results = new StudentMark[ StudentCount ]; //зачем этот массив для решения задачи?
-            for ( int i = 0 ; i < StudentCount ; i++ ) {
-                int mark = minMark + (int) (Math.random ( ) * maxMark);
-                results[ i ] = new StudentMark ( mark );
-                sumMark += results[ i ].mark;
+            int studentCount = 5;
+            for ( int i = 0 ; i < studentCount ; i++ ) {
+                int mark = passExam(minMark, maxMark);
+                sumMark += mark;
             }
-            avgMark = sumMark / StudentCount;
+            float avgMark = sumMark / studentCount;
             System.out.println ();
             System.out.println ( "Average mark for the group is: " + avgMark );
         }
         if (mode == 2) {
             float sumMark = 0;
-            float avgMark = 0;
-            int StudentCount = 5;
+            int studentCount = 5;
             int ticketsCount = 10;
-            Student[] allstudents = new Student[ StudentCount ]; //зачем этот массив для решения задачи?
-            Ticket[] alltickets = new Ticket[ ticketsCount ];
-            StudentMark[] results = new StudentMark[ StudentCount ];
-            StudentTicket[] studenttickets = new StudentTicket[StudentCount];
+            Ticket[] allTickets = new Ticket[ ticketsCount ];
+            StudentMark[] results = new StudentMark[ studentCount ];
+            StudentTicket[] studentTickets = new StudentTicket[studentCount];
             for ( int i = 0 ; i < ticketsCount ; i++ ) {
-                alltickets[ i ] = new Ticket ( i+1, "To be or not to be #"+(i+1)+"?" );
+                allTickets[ i ] = new Ticket ( i+1, "To be or not to be #"+(i+1)+"?" );
             }
-            for ( int i = 0 ; i < StudentCount ; i++ ) {
-                studenttickets[ i ] = new StudentTicket ( 0 );
-            }
+            nullTickets(studentCount, studentTickets);
             System.out.println ();
-            for ( int i = 0 ; i < StudentCount ; i++ ) {
-                int ticket = 1 + (int) (Math.random ( ) * ticketsCount);
-                while (studenttickets[i].id == 0) {
-                    int check = 0;
-                    for (int j = 0; j < StudentCount; j++){
-                        if (ticket == studenttickets[j].id){
-                            check++;
-                        }
-                    }
+            for ( int i = 0 ; i < studentCount ; i++ ) {
+                int ticket = getTicket(ticketsCount);
+                while (studentTickets[i].id == 0) {
+                    int check = checkTicket(studentCount, studentTickets, ticket);
                     if (check==0){
-                        studenttickets[ i ] = new StudentTicket ( ticket );
+                        studentTickets[ i ] = new StudentTicket ( ticket );
                     }
                     else {
-                        ticket = 1 + (int) (Math.random ( ) * ticketsCount);
+                        ticket = getTicket(ticketsCount);
                     }
                 }
-                int mark = minMark + (int) (Math.random ( ) * maxMark);
-                results[ i ] = new StudentMark ( mark );
-                sumMark += results[ i ].mark;
+                sumMark = saveResultsGetSum(minMark, maxMark, sumMark, results, i);
                 System.out.print ( "Student #"+(i+1)+" passed exam with a mark " + results[i].mark +". ");
-                System.out.println ( "Ticket was: " + studenttickets[i].id+". Question was: "+alltickets[studenttickets[i].id-1].question);
+                System.out.println ( "Ticket was: " + studentTickets[i].id+". Question was: "+allTickets[studentTickets[i].id-1].question);
             }
-            avgMark = sumMark / StudentCount;
+            float avgMark = sumMark / studentCount;
             System.out.println ();
             System.out.println ( "Average mark for the group is: " + avgMark );
-            //for (int i = 0; i<StudentCount; i++) {
-            //    System.out.print ( "Student #"+(i+1)+" passed exam with a mark " + results[i].mark +". ");
-            //    System.out.println ( "Ticket was: " + studenttickets[i].id+". Question was: "+alltickets[studenttickets[i].id-1].question);
-            //}
         }
         if (mode == 3) {
-            int StudentCount = 5;
+            int studentCount = 5;
             int ticketsCount = 10;
             int groupCount = 2;
-            Student[][] allstudents = new Student[ groupCount ][ StudentCount ];
-            Ticket[] alltickets = new Ticket[ ticketsCount ];
-            StudentMark[][] results = new StudentMark[ groupCount ] [ StudentCount ];
-            StudentTicket[][] studenttickets = new StudentTicket[ groupCount ] [ StudentCount ];
+            Student[][] allStudents = new Student[ groupCount ][ studentCount ];
+            StudentMark[][] results = new StudentMark[ groupCount ] [ studentCount ];
+            StudentTicket[][] studentTickets = new StudentTicket[ groupCount ] [ studentCount ];
             Group[] allgroups = new Group[ groupCount ];
-            for ( int i = 0 ; i < ticketsCount ; i++ ) {
-                alltickets[ i ] = new Ticket ( i + 1 , "To be or not to be #" + (i + 1) + "?" );
-            }
             for ( int j = 0 ; j < groupCount ; j++ ) {
-                for ( int i = 0 ; i < StudentCount ; i++ ) {
-                    studenttickets[ j ][ i ] = new StudentTicket ( 0 );
-                }
+                nullTickets(studentCount, studentTickets[ j ]);
             }
             for ( int i = 0 ; i < groupCount ; i++ ) {
                 allgroups[ i ] = new Group ( "Group #" + (i + 1) );
             }
             for ( int gr = 0 ; gr < groupCount ; gr++ ) {
                 float sumMark = 0;
-                float avgMark = 0;
-                for ( int i = 0 ; i < StudentCount ; i++ ) {
-                    allstudents[ gr ][ i ] = new Student ( "Name-"+(gr+1)+"-"+(i+1), "Lastname-"+(gr+1)+"-"+(i+1) );
-                    int ticket = 1 + (int) (Math.random ( ) * ticketsCount);
-                    while (studenttickets[ gr ][ i ].id == 0) {
-                        int check = 0;
-                        for ( int j = 0 ; j < StudentCount ; j++ ) {
-                            if (ticket == studenttickets[ gr ][ j ].id) {
-                                check++;
-                            }
-                        }
+                for ( int i = 0 ; i < studentCount ; i++ ) {
+                    allStudents[ gr ][ i ] = new Student ( "Name-"+(gr+1)+"-"+(i+1), "Lastname-"+(gr+1)+"-"+(i+1) );
+                    int ticket = getTicket(ticketsCount);
+                    while (studentTickets[ gr ][ i ].id == 0) {
+                        int check = checkTicket(studentCount, studentTickets[gr], ticket);
                         if (check == 0) {
-                            studenttickets[ gr ][ i ] = new StudentTicket ( ticket );
+                            studentTickets[ gr ][ i ] = new StudentTicket ( ticket );
                         } else {
-                            ticket = 1 + (int) (Math.random ( ) * ticketsCount);
+                            ticket = getTicket(ticketsCount);
                         }
                     }
-                    int mark = minMark + (int) (Math.random ( ) * maxMark);
-                    results[ gr ][ i ] = new StudentMark ( mark );
-                    sumMark += results[ gr ][ i ].mark;
-                    //System.out.print ( "Student " + allstudents[gr][i].name);
-                    //System.out.println ( ". Mark is " + results[gr][i].mark);
+                    sumMark = saveResultsGetSum(minMark, maxMark, sumMark, results[ gr ], i);
                 }
-                avgMark = sumMark / StudentCount;
+                float avgMark = sumMark / studentCount;
                 System.out.println ();
                 System.out.println ( "Average mark for the "+allgroups[gr].number+" is: " + avgMark );
-                int indexmin=0;
-                int indexmax=0;
+                int indexMin=0;
                 int min=0;
+                int indexMax=0;
                 int max=0;
-                for (int i = 1; i < StudentCount; i++) {
-                    if (results[gr][i].mark < results[gr][indexmin].mark) {
-                        indexmin = i;
+                for (int i = 1; i < studentCount; i++) {
+                    if (results[gr][i].mark < results[gr][indexMin].mark) {
+                        indexMin = i;
                         min=results[gr][i].mark;}
                     else {
-                        min=results[gr][indexmin].mark;
-                        }
+                        min=results[gr][indexMin].mark;
+                    }
 
-                    if (results[gr][i].mark > results[gr][indexmax].mark){
-                        indexmax = i;
+                    if (results[gr][i].mark > results[gr][indexMax].mark){
+                        indexMax = i;
                         max=results[gr][i].mark;
                     }
                     else {
-                        max=results[gr][indexmax].mark;
+                        max=results[gr][indexMax].mark;
                     }
                 }
-                //System.out.println ( "IndexMin "+indexmin+ " Min "+min);
-                //System.out.println ( "IndexMax "+indexmax+ " Max "+max);
                 System.out.println ( "   The best student(s) in the " + allgroups[gr].number + ": ");
-                for (int i = 0; i < StudentCount; i++) {
-                    if (max == results[ gr ][ i ].mark) {
-                        System.out.println ( "        - " + allstudents[ gr ][ i ].name + " " + allstudents[ gr ][ i ].lastName );
-                    }
-                }
+                printStudentByMark(studentCount, max, results[gr], "        - ", allStudents[gr]);
                 if ( min != max ) {
                     System.out.println ( "   The worst student(s) in the " + allgroups[gr].number + ": ");
-                    for ( int i = 0 ; i < StudentCount ; i++ ) {
-                        if (min == results[ gr ][ i ].mark) {
-                            System.out.println ( "        - " + allstudents[ gr ][ i ].name + " " + allstudents[ gr ][ i ].lastName );
-                        }
-                    }
+                    printStudentByMark(studentCount, min, results[ gr ], "        - ", allStudents[ gr ]);
                 }
             }
-            int GRindexmin=0;
-            int GRindexmax=0;
-            int STindexmax=0;
-            int STindexmin=0;
-            int totalmin=0;
-            int totalmax=0;
+            int grIndexMin=0;
+            int grIndexMax=0;
+            int stIndexMax=0;
+            int stIndexMin=0;
+            int totalMin=0;
+            int totalMax=0;
             for (int j = 0; j < groupCount; j++) {
-                for ( int i = 1 ; i < StudentCount ; i++ ) {
-                    if (results[ j ][ i ].mark < results[ GRindexmin ][ STindexmin ].mark) {
-                        STindexmin = i;
-                        GRindexmin = j;
-                        totalmin = results[ j ][ i ].mark;
+                for ( int i = 1 ; i < studentCount ; i++ ) {
+                    if (results[ j ][ i ].mark < results[ grIndexMin ][ stIndexMin ].mark) {
+                        stIndexMin = i;
+                        grIndexMin = j;
+                        totalMin = results[ j ][ i ].mark;
                     } else {
-                        totalmin = results[ GRindexmin ][ STindexmin ].mark;
+                        totalMin = results[ grIndexMin ][ stIndexMin ].mark;
                     }
 
-                    if (results[ j ][ i ].mark > results[ GRindexmax ][ STindexmax ].mark) {
-                        STindexmax = i;
-                        GRindexmax = j;
-                        totalmax = results[ j ][ i ].mark;
+                    if (results[ j ][ i ].mark > results[ grIndexMax ][ stIndexMax ].mark) {
+                        stIndexMax = i;
+                        grIndexMax = j;
+                        totalMax = results[ j ][ i ].mark;
                     } else {
-                        totalmax = results[ GRindexmax ][ STindexmax ].mark;
+                        totalMax = results[ grIndexMax ][ stIndexMax ].mark;
                     }
                 }
             }
             System.out.println ();
             System.out.println ( "Totally the best student(s):");
             for (int j = 0; j < groupCount; j++) {
-                for ( int i = 0 ; i < StudentCount ; i++ ) {
-                    if (totalmax == results[ j ][ i ].mark) {
-                        System.out.println ( "     - " + allstudents[ j ][ i ].name + " " + allstudents[ j ][ i ].lastName );
-                    }
-                }
+                printStudentByMark(studentCount, totalMax, results[ j ], "     - ", allStudents[ j ]);
             }
-            //System.out.println ("ttlMAX "+totalmax);
-            //System.out.println ("ttlMIN "+totalmin);
-            if (totalmin != totalmax) {
+            if (totalMin != totalMax) {
                 System.out.println ();
                 System.out.println ( "Totally the worst student(s):" );
                 for (int j = 0; j < groupCount; j++) {
-                    for ( int i = 0 ; i < StudentCount ; i++ ) {
-                        if (totalmin == results[ j ][ i ].mark) {
-                                 System.out.println ( "     - " + allstudents[ j ][ i ].name + " " + allstudents[ j ][ i ].lastName );
-                        }
-                    }
+                    printStudentByMark(studentCount, totalMin, results[ j ], "     - ", allStudents[ j ]);
                 }
             }
         }
+    }
+
+    private static void printStudentByMark(int studentCount, int value, StudentMark[] result, String s, Student[] allStudent) {
+        for (int i = 0; i < studentCount; i++) {
+            if (value == result[i].mark) {
+                System.out.println(s + allStudent[i].name + " " + allStudent[i].lastName);
+            }
+        }
+    }
+
+    private static void nullTickets(int studentCount, StudentTicket[] studentTickets) {
+        for (int i = 0; i < studentCount; i++) {
+            studentTickets[i] = new StudentTicket(0);
+        }
+    }
+
+    private static float saveResultsGetSum(int minMark, int maxMark, float sumMark, StudentMark[] results, int i) {
+        int mark = passExam(minMark, maxMark);
+        results[i] = new StudentMark(mark);
+        sumMark += results[i].mark;
+        return sumMark;
+    }
+
+    private static int checkTicket(int studentCount, StudentTicket[] studentTickets, int ticket) {
+        int check = 0;
+        for (int j = 0; j < studentCount; j++) {
+            if (ticket == studentTickets[j].id) {
+                check++;
+            }
+        }
+        return check;
+    }
+
+    private static int getTicket(int ticketsCount) {
+        return 1 + (int) (Math.random() * ticketsCount);
+    }
+
+    private static int passExam(int minMark, int maxMark) {
+        return minMark + (int) (Math.random() * maxMark);
     }
 }
 
@@ -209,7 +193,6 @@ class Student {
     String name;
     String lastName;
 
-    public
     Student ( String name , String lastName ) {
         this.name = name;
         this.lastName = lastName;
@@ -219,7 +202,6 @@ class Student {
 class StudentMark {
     int mark;
 
-    public
     StudentMark ( int mark ) {
         this.mark = mark;
     }
@@ -229,7 +211,6 @@ class Ticket {
     int id;
     String question;
 
-    public
     Ticket ( int id, String question ) {
         this.id = id;
         this.question = question;
@@ -239,7 +220,6 @@ class Ticket {
 class StudentTicket {
     int id;
 
-    public
     StudentTicket ( int id ) {
         this.id = id;
     }
@@ -248,7 +228,6 @@ class StudentTicket {
 class Group {
     String number;
 
-    public
     Group ( String number ) {
         this.number = number;
     }
