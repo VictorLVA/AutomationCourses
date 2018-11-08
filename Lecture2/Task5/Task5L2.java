c485bf94c66ba5f3855696b307ca7d47210b1db4
Victor <victorl@playtika.com>
2018-11-09 01:57:18 +0300

    comments fixes

--- Lecture2/Task5/Task5L2.java
+++ Lecture2/Task5/Task5L2.java
@@ -20,108 +20,102 @@ public class Task5L2 {
             lastname = getWord(console);
         }
         System.out.println("\nNow try to solve secret name and lastname ;)");
-        List<String> listOfWords = new ArrayList<>();
-        List<String> listOfLetters = new ArrayList<>();
-        char[] nameArray = name.toCharArray();
-        char[] lastnameArray = lastname.toCharArray();
-        char[] starNameArray = new char[name.length()];
-        for (int i = 0; i < starNameArray.length; i++) {
-            starNameArray[i] = '*';
+        GameData gameData = new GameData ();
+        gameData.initGameData(name, lastname);
+        for (int i = 0; i < name.length (); i++) {
+            gameData.starNameArray[i] = '*';
         }
-        char[] starLastnameArray = new char[lastname.length()];
-        for (int i = 0; i < starLastnameArray.length; i++) {
-            starLastnameArray[i] = '*';
+        for (int i = 0; i < lastname.length (); i++) {
+            gameData.starLastnameArray[i] = '*';
         }
-        while (!Arrays.equals(nameArray, starNameArray) || !Arrays.equals(lastnameArray, starLastnameArray)) {
+        while (checkNotFinished(gameData)) {
             System.out.print("\nYour choice: ");
             String choice = reader.readLine();
-            if (listOfWords.contains(choice) || listOfLetters.contains(choice)) {
+            if (gameData.listOfWords.contains(choice) || gameData.listOfLetters.contains(choice)) {
                 System.out.println(MESSAGE_AGAIN);
                 continue;
             }
-            if (isChoiceAlreadySolved(nameArray, starNameArray, choice, name)) {
+            if (checkChoiceAlreadySolved(gameData, choice, name, lastname)) {
                 System.out.println(MESSAGE_AGAIN);
                 continue;
             }
-            if (isChoiceAlreadySolved(lastnameArray, starLastnameArray, choice, lastname)) {
-                System.out.println(MESSAGE_AGAIN);
-                continue;
-            }
-            if (isInvalidInput(choice, name, lastname)) {
+            if (checkInvalidInput(choice, name, lastname)) {
                 System.out.println("You have to enter one letter or a whole name/lastname. Please try again.");
                 continue;
             }
             if (choice.length() != 1) {
-                wordProcessing(nameArray, starNameArray, choice, name, lastnameArray, starLastnameArray, lastname, listOfWords);
+                wordProcessing(choice, name, lastname, gameData);
             }
             if (choice.length() == 1) {
-                letterProcessing(nameArray, starNameArray, choice, lastnameArray, starLastnameArray, listOfLetters);
+                letterProcessing(choice, gameData);
             }
-            finishGameMessage(Arrays.equals(nameArray, starNameArray), Arrays.equals(lastnameArray, starLastnameArray));
+            finishGameMessage(!checkNotFinished(gameData));
         }
     }
 
-    private static void printStar(char[] starNameArray, char[] starLastnameArray) {
-        for (char eachStarNameChar : starNameArray) {
+    private static void printStar(GameData gameData) {
+        for (char eachStarNameChar : gameData.starNameArray) {
             System.out.print(eachStarNameChar);
         }
         System.out.print(" ");
-        for (char eachStarLastnameChar : starLastnameArray) {
+        for (char eachStarLastnameChar : gameData.starLastnameArray) {
             System.out.print(eachStarLastnameChar);
         }
         System.out.println();
     }
 
-    private static void printRightMove(char[] starNameArray, char[] starLastnameArray) {
+    private static void printRightMove(GameData gameData) {
         System.out.println("Right. Please see the interim result: ");
-        printStar(starNameArray , starLastnameArray);
+        printStar(gameData);
     }
 
     private static void printWrongMove() {
         System.out.println("Oops... It's a mistake. You can better!!! Let's continue.");
     }
 
-    private static void printWholeWord(char[] starNameArray , char[] starLastnameArray) {
+    private static void printWholeWord(GameData gameData) {
         System.out.println("Wow, congratulations! The word(s) is(are) solved!");
-        printStar(starNameArray, starLastnameArray);
+        printStar(gameData);
     }
 
-    private static void letterMessageValidation(boolean isRightMoveForName, boolean isRightMoveForLastname, char[] nameArray, char[] lastnameArray, char[] starNameArray, char[] starLastnameArray) {
+    private static void letterMessageValidation(boolean isRightMoveForName, boolean isRightMoveForLastname, GameData gameData) {
         if (isRightMoveForName && isRightMoveForLastname) {
-            if (Arrays.equals(nameArray, starNameArray) || Arrays.equals(lastnameArray, starLastnameArray)) {
-                printWholeWord(starNameArray, starLastnameArray);
+            if (Arrays.equals(gameData.nameArray, gameData.starNameArray)
+                    || Arrays.equals(gameData.lastnameArray, gameData.starLastnameArray)) {
+                printWholeWord(gameData);
             }
             else {
-                printRightMove(starNameArray, starLastnameArray);
+                printRightMove(gameData);
             }
         }
         else if (isRightMoveForName) {
-            if (Arrays.equals(nameArray, starNameArray)) {
-                printWholeWord(starNameArray, starLastnameArray);
+            if (Arrays.equals(gameData.nameArray, gameData.starNameArray)) {
+                printWholeWord(gameData);
             }
             else {
-                printRightMove(starNameArray, starLastnameArray);
+                printRightMove(gameData);
             }
         }
         else if (isRightMoveForLastname) {
-            if (Arrays.equals(lastnameArray, starLastnameArray)) {
-                printWholeWord(starNameArray, starLastnameArray);
+            if (Arrays.equals(gameData.lastnameArray, gameData.starLastnameArray)) {
+                printWholeWord(gameData);
             }
-            else printRightMove(starNameArray, starLastnameArray);
+            else printRightMove(gameData);
         }
         else {
             printWrongMove();
         }
     }
 
-    private static void finishGameMessage(boolean isNameSolved, boolean isLastnameSolved) {
-        if (isNameSolved && isLastnameSolved) {
+    private static void finishGameMessage(boolean isFinished) {
+        if (isFinished) {
             System.out.print("\nWonderful!!! You did it!!! See you later...\n");
         }
     }
 
-    private static boolean isLetterRightMove(String choice, int arraylength, char[] array, char[] starArray, boolean isLetterRightMove) {
-        for (int i = 0; i < arraylength; i++) {
+    private static boolean checkLetterRightMove(String choice, char[] array, char[] starArray) {
+        boolean isLetterRightMove = false;
+        for (int i = 0; i < array.length; i++) {
             if (choice.equalsIgnoreCase(Character.toString(array[i]))) {
                 starArray[i] = array[i];
                 isLetterRightMove = true;
@@ -130,48 +124,48 @@ public class Task5L2 {
         return isLetterRightMove;
     }
 
-    private static void letterProcessing(char[] nameArray, char[] starNameArray, String choice, char[] lastnameArray, char[] starLastnameArray, List <String> listOfLetters) {
+    private static void letterProcessing(String choice, GameData gameData) {
         boolean isRightMoveForName = false;
         boolean isRightMoveForLastname = false;
-        if (!Arrays.equals(nameArray , starNameArray)) {
-            isRightMoveForName = isLetterRightMove(choice, nameArray.length, nameArray, starNameArray, isRightMoveForName);
+        if (!Arrays.equals(gameData.nameArray, gameData.starNameArray)) {
+            isRightMoveForName = checkLetterRightMove(choice, gameData.nameArray, gameData.starNameArray);
         }
-        if (!Arrays.equals(lastnameArray , starLastnameArray)) {
-            isRightMoveForLastname = isLetterRightMove(choice, lastnameArray.length, lastnameArray, starLastnameArray, isRightMoveForLastname);
+        if (!Arrays.equals(gameData.lastnameArray, gameData.starLastnameArray)) {
+            isRightMoveForLastname = checkLetterRightMove(choice, gameData.lastnameArray, gameData.starLastnameArray);
         }
-        listOfLetters.add(choice);
-        letterMessageValidation(isRightMoveForName, isRightMoveForLastname, nameArray, lastnameArray, starNameArray, starLastnameArray);
+        gameData.listOfLetters.add(choice);
+        letterMessageValidation(isRightMoveForName, isRightMoveForLastname, gameData);
     }
 
-    private static void wordMessageValidation(boolean isWordOpened, char[] starNameArray, char[] starLastnameArray) {
+    private static void wordMessageValidation(boolean isWordOpened, GameData gameData) {
         if (isWordOpened) {
-            printWholeWord(starNameArray, starLastnameArray);
+            printWholeWord(gameData);
         }
         else {
             printWrongMove();
         }
     }
 
-    private static boolean arrayCopyAndTrue(char[] array, char[] starArray) {
+    private static boolean checkArrayCopy(char[] array, char[] starArray) {
         System.arraycopy(array, 0, starArray, 0, array.length);
         return true;
     }
 
-    private static void wordProcessing(char[] nameArray, char[] starNameArray, String choice, String name, char[] lastnameArray, char[] starLastnameArray, String lastname, List<String> listOfWords) {
+    private static void wordProcessing(String choice, String name, String lastname, GameData gameData) {
         boolean isWordOpened = false;
         if (choice.equalsIgnoreCase(name)) {
-            isWordOpened = arrayCopyAndTrue(nameArray, starNameArray);
+            isWordOpened = checkArrayCopy(gameData.nameArray, gameData.starNameArray);
         }
         if (choice.equalsIgnoreCase(lastname)) {
-            isWordOpened = arrayCopyAndTrue(lastnameArray, starLastnameArray);
+            isWordOpened = checkArrayCopy(gameData.lastnameArray, gameData.starLastnameArray);
         }
-        listOfWords.add(choice);
-        wordMessageValidation(isWordOpened, starNameArray, starLastnameArray);
+        gameData.listOfWords.add(choice);
+        wordMessageValidation(isWordOpened, gameData);
     }
 
     private static String getWord(Console console) {
         String word = null;
-        console.printf("Please enter a word (name or lastname):");
+        console.printf("Please enter a word ("+ REGEX_NAME_LASTNAME + "):");
         char[] secret = console.readPassword();
         String check = new String(secret);
         if (check.matches(REGEX_NAME_LASTNAME)) {
@@ -180,15 +174,38 @@ public class Task5L2 {
         return word;
     }
 
-    private static boolean isChoiceAlreadySolved (char[] array, char[] starArray, String choice, String word) {
-        return Arrays.equals(array, starArray) && choice.equalsIgnoreCase(word);
+    private static boolean checkChoiceAlreadySolved(GameData gameData, String choice, String name, String lastname) {
+        return Arrays.equals(gameData.nameArray, gameData.starNameArray) && choice.equalsIgnoreCase(name)
+                || Arrays.equals(gameData.lastnameArray, gameData.starLastnameArray) && choice.equalsIgnoreCase(lastname);
     }
 
-    private static boolean isInvalidInput (String choice, String name, String lastname) {
+    private static boolean checkInvalidInput(String choice, String name, String lastname) {
         return choice.length() != name.length()
                 && choice.length() != lastname.length()
                 && choice.length() != 1
                 || !choice.matches(REGEX_NAME_LASTNAME);
     }
 
+    private static boolean checkNotFinished(GameData gameData) {
+        return !Arrays.equals(gameData.nameArray, gameData.starNameArray)
+                || !Arrays.equals(gameData.lastnameArray, gameData.starLastnameArray);
+    }
+}
+
+class GameData {
+    List<String> listOfWords;
+    List<String> listOfLetters;
+    char[] nameArray;
+    char[] lastnameArray;
+    char[] starNameArray;
+    char[] starLastnameArray;
+
+    void initGameData(String name, String lastname) {
+        this.listOfWords = new ArrayList<>();
+        this.listOfLetters = new ArrayList<>();
+        this.nameArray = name.toCharArray();
+        this.lastnameArray = lastname.toCharArray();
+        this.starNameArray = new char[name.length()];
+        this.starLastnameArray = new char[lastname.length()];
+    }
 }
