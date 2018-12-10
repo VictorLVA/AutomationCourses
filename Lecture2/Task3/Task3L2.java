ae55570fe91b9e4613bb90322761bbcc8980a2a5
Victor <victorl@playtika.com>
2018-12-10 23:41:42 +0300

    transition to java8

--- Lecture2/Task3/Task3L2.java
+++ Lecture2/Task3/Task3L2.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Task3L2 {
 
+    private static final String SYMBOL_BLANK_SPACE = " ";
+    private static final String SYMBOL_CONTOUR = "*";
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Please enter a rhombus size: ");
@@ -13,49 +16,48 @@ public class Task3L2 {
             return;
         }
         if (size < 2) {
-            System.out.println("You have entered an invalid rhombus size (should be 3 at least). Please try again");
+            System.out.println("You have entered an invalid rhombus size (should be 2 at least). Please try again");
             return;
         }
         drawRhombus(size);
     }
 
     private static void drawRhombus(int size) {
-        String space = " ";
         int innerSpaceCount = 1;
         for (int i = 1; i < size * 2; i++) {
             if (i == 1) {
                 for (int j = 1; j < size; j++) {
-                    System.out.print(space);
+                    System.out.print(SYMBOL_BLANK_SPACE);
                 }
-                System.out.println("*");
+                System.out.println(SYMBOL_CONTOUR);
             }
             if (i > 1 && i < size) {
                 for (int j = 0; j < size - i; j++) {
-                    System.out.print(space);
+                    System.out.print(SYMBOL_BLANK_SPACE);
                 }
-                System.out.print("*");
+                System.out.print(SYMBOL_CONTOUR);
                 for (int j = 0; j < innerSpaceCount; j++) {
-                    System.out.print(space);
+                    System.out.print(SYMBOL_BLANK_SPACE);
                 }
-                System.out.println("*");
+                System.out.println(SYMBOL_CONTOUR);
                 innerSpaceCount += 2;
             }
             if (i >= size && i < size * 2 - 1) {
                 for (int j = 0; j < i - size; j++) {
-                    System.out.print(space);
+                    System.out.print(SYMBOL_BLANK_SPACE);
                 }
-                System.out.print("*");
+                System.out.print(SYMBOL_CONTOUR);
                 for (int j = 0; j < innerSpaceCount; j++) {
-                    System.out.print(space);
+                    System.out.print(SYMBOL_BLANK_SPACE);
                 }
-                System.out.println("*");
+                System.out.println(SYMBOL_CONTOUR);
                 innerSpaceCount -= 2;
             }
             if (i == size * 2 - 1) {
                 for (int j = 0; j < i - size; j++) {
-                    System.out.print(space);
+                    System.out.print(SYMBOL_BLANK_SPACE);
                 }
-                System.out.print("*");
+                System.out.print(SYMBOL_CONTOUR);
             }
         }
     }
